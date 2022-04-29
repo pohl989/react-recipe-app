1a4086bfab19598af3952094831daf4188f918e4
pohl989 <pohl989@gmail.com>
2022-04-28 22:02:24 -0500

    base eslint and defaults

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  'root': true,
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'react/react-in-jsx-scope': ['off'],
+    'no-unused-vars': ['warn',
+    ],
+    'semi': ['error', 'never'],
+    'require-jsdoc': ['error', {
+      'require': {
+        'FunctionDeclaration': false,
+        'MethodDefinition': false,
+        'ClassDeclaration': false,
+        'ArrowFunctionExpression': false,
+        'FunctionExpression': false,
+      },
+    }],
+    'max-len': ['warn', {'code': 140}],
+  },
+  'settings': {
+    'react': {
+      'createClass': 'createReactClass',
+      'pragma': 'React',
+      'fragment': 'Fragment',
+      'version': 'detect',
+      'flowVersion': '0.53',
+    },
+  },
+}
